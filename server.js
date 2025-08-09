4b8c7370eb8523c3e14ccc74d683b7e14f10f2bb
Julio Ramírez <julioramirezs2008@gmail.com>
2025-08-09 03:08:49 -0600

    version anterior funcional

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const cors = require('cors');
 const app = express();
 const server = require('http').Server(app);
 const { ExpressPeerServer } = require('peer');
-const io = require('socket.io')(server);
 
 const corsOptions = {
     origin: 'https://meet-front.onrender.com',
@@ -13,21 +12,26 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json());
 
+// Configuración de Socket.IO con las opciones de CORS
+const io = require('socket.io')(server, {
+    cors: corsOptions
+});
+
 const peerServer = ExpressPeerServer(server, {
     path: '/myapp'
 });
-
 app.use('/peerjs', peerServer);
 
 io.on('connection', socket => {
-    socket.on('join-room', (roomId, userId) => {
-        console.log(`[Socket] Usuario ${userId} se unió a la sala ${roomId}.`);
+    socket.on('join-room', (roomId, userId, userName) => {
+        console.log(`[Socket] Usuario ${userName} (${userId}) se unió a la sala ${roomId}.`);
         socket.join(roomId);
 
-        socket.to(roomId).emit('user-connected', userId);
+        // Notifica a los demás en la sala sobre el nuevo usuario
+        socket.to(roomId).emit('user-connected', { userId, userName });
 
         socket.on('disconnect', () => {
-            console.log(`[Socket] Usuario ${userId} se desconectó.`);
+            console.log(`[Socket] Usuario ${userName} (${userId}) se desconectó.`);
             socket.to(roomId).emit('user-disconnected', userId);
         });
     });
@@ -35,5 +39,5 @@ io.on('connection', socket => {
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log(`Servidor escuchando en el puerto ${PORT}`);
+    console.log(`Servidor de señalización y WebRTC escuchando en el puerto ${PORT}`);
 });
